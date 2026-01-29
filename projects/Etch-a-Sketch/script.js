abd910cf6106a7b6f8c3ff8bda12444e3577a7cd
rootadbc <210502962+rootadbc@users.noreply.github.com>
2026-01-29 06:17:46 +0100

    PHASE1: finished phase 1 , displayed the box

--- projects/Etch-a-Sketch/script.js
+++ projects/Etch-a-Sketch/script.js
@@ -1,4 +1,13 @@
-// File: projects/Etch-a-Sketch/script.js
-const SIZE = 16;
-const grid = Array.from({length: SIZE}, () => Array(SIZE).fill(' '));
+// 
+// Find the outer box in the HTML where the grid will live
+const container = document.querySelector('.gridContainer');
 
+// We want 16 x 16 = 256 small squares
+for (let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement('div'); // make a new <div> for each square
+    square.classList.add('square'); // Give it a class so CSS can style it
+    container.appendChild(square);  // Put this square inside the container
+
+}
+
+console.log("Hello, Etch-a-Sketch!");
