c74d8e77f9b56652ff4fb35d4b45dddae3e8fb56
rootadbc <210502962+rootadbc@users.noreply.github.com>
2026-01-29 07:25:07 +0100

    BUG: phase2, mouse event hovering

--- projects/Etch-a-Sketch/script.js
+++ projects/Etch-a-Sketch/script.js
@@ -1,4 +1,4 @@
-// 
+
 // Find the outer box in the HTML where the grid will live
 const container = document.querySelector('.gridContainer');
 
@@ -10,4 +10,20 @@ for (let i = 0; i < 16 * 16; i++) {
 
 }
 
+
+//Select all the small grid squares
+const squares = document.querySelectorAll('.square');
+
+// Loop through each square and add hover listener
+squares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+        console.log('hovered');
+        square.style.backgroundColor = 'black';
+    });
+});
+
+
+
 console.log("Hello, Etch-a-Sketch!");
+
+const testSquares = document.querySelectorAll('.square');
